4d406049803d1c17ee1b7c4cdbf3be4edafc3b43
Oscar Agerskov <oscar.agerskov@gmail.com>
2022-09-29 23:52:45 +0200

    A1

--- src/CDIO.java
+++ src/CDIO.java
@@ -1,17 +1,30 @@
+import java.util.Random;
 public class CDIO {
     public static void main(String[] args){
-        System.out.println("hello cdio");
-        System.out.println("Hej med jer");
 
+    Random ranNum = new Random();
 
-
-
-
-
-
-
-
-
-
+    int totalSpillerA=0;
+    int totalSpillerB=0;
+    int randomNummer=0;
+       while(totalSpillerA <=50 || totalSpillerB<=50  ) {
+            randomNummer = ranNum.nextInt(6) + 1;
+            totalSpillerA = totalSpillerA + randomNummer;
+            System.out.println(randomNummer + " ");
+            randomNummer = ranNum.nextInt(6) + 1;
+            totalSpillerB = totalSpillerB + randomNummer;
+            System.out.println(randomNummer);
+            System.out.print(" ");
+        }
+        if(totalSpillerA > totalSpillerB) {
+            System.out.println("");
+            System.out.println("A vinder Total: " + totalSpillerA);
+        } else if (totalSpillerB > totalSpillerA) {
+            System.out.println("");
+            System.out.println("B vinder Total: " + totalSpillerA);
+        } else {
+            System.out.println("");
+            System.out.println("Uafgjort" + totalSpillerA);
+        }
     }
 }
